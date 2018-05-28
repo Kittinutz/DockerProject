6a0262767769b469fb9608137f407bebffc02d0a
Kittinut Pramhan <knightza94@gmail.com>
2018-05-29 00:07:43 +0700

    add on nodejs

---
+++ nodejs/server.js
@@ -0,0 +1,29 @@
+const express =  require( 'express' );
+const app = express();
+const Html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello Nginx Proxy</title>
+    <style>
+        body{
+        text-align: center;
+        color: tomato;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hello My nginx this is Awesome Website Right ?</h1>
+    <p>We will do SSL Letsencrypt Later </p>
+    <p>And now you are Proxy on nodejs ports 3000 </p>
+    <img src="https://goo.gl/cZzBcf" alt="">
+</body>
+</html>
+`
+app.use('/',(req,res)=>{
+    res.send(Html);
+})
+app.listen(3000,()=>{
+    console.log('server is listen on port 3000')
+})
